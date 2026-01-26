7841710e4affe2b0d495646b3d13285f62b090cf
boiuW0Tm8 <30941775+boiuW0Tm8@users.noreply.github.com>
2026-01-25 21:57:04 -0500

    mobile fix attempt

--- app/almanac/layout.tsx
+++ app/almanac/layout.tsx
@@ -5,7 +5,10 @@ export default function AlmanacLayout({
 }) {
   return (
     <section className="bg-neutral-50 min-h-screen">
-      {children}
+      <div className="max-w-4xl mx-auto px-6 py-20
+        text-neutral-900 sm:text-neutral-700">
+        {children}
+      </div>
     </section>
   );
 }
