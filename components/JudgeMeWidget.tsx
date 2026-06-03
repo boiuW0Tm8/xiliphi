f61fe23bb0719869cc55d91bb297587e0222dcf2
Xiwen Liu <kevinster.liu@gmail.com>
2026-06-03 00:11:05 -0400

    more testing

--- components/JudgeMeWidget.tsx
+++ components/JudgeMeWidget.tsx
@@ -1,6 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function JudgeMeBadge({ productId }: { productId: string }) {
   const numericId = productId.replace("gid://shopify/Product/", "");
+
+  useEffect(() => {
+    const s = document.createElement("script");
+    s.src = `https://cdnwidget.judge.me/widget_preloader.js?_=${Date.now()}`;
+    s.async = true;
+    s.setAttribute("data-cfasync", "false");
+    document.body.appendChild(s);
+    return () => {
+      s.remove();
+    };
+  }, [numericId]);
+
   return <div className="jdgm-widget jdgm-preview-badge" data-id={numericId} />;
 }
