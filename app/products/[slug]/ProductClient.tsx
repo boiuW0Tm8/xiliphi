b77407d39e222e11d439df23b84fd22bd29ea49c
Xiwen Liu <kevinster.liu@gmail.com>
2026-06-24 06:40:51 -0400

    another shipping change

--- app/products/[slug]/ProductClient.tsx
+++ app/products/[slug]/ProductClient.tsx
@@ -459,7 +459,7 @@ export default function ProductClient({ product, from }: { product: any; from: s
               {buttonLabel()}
             </button>
 
-            <p className="text-xs text-center mt-4">Free shipping on orders over $10!</p>
+            <p className="text-xs text-center mt-4">Free shipping on orders over $30!</p>
 
             <div className="border-t border-neutral-200 my-8" />
 
